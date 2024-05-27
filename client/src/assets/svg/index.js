e9252a1786d122909f6b14797f14f035ba32cee0
Geethosh G V <133562856+GeethoshGv@users.noreply.github.com>
2024-05-27 08:24:10 +0530

    Add files via upload

--- client/src/assets/svg/index.js
+++ client/src/assets/svg/index.js
@@ -4,5 +4,8 @@ import location from './location.svg'
 import bed from './bed.svg'
 import parking from './parking.svg'
 import user from './user.svg'
+import college from './college.svg'
+import sale from './sale.svg'
+import hospital from './hospital.svg'
 
-export { search, user, bath, bed, location, parking }
+export { search, user, bath, bed, location, parking, hospital, sale, college }
